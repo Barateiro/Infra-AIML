1fd1d11c6292d9e6e70d3945c70cbc55929d9d89
Barateiro <77500351+Barateiro@users.noreply.github.com>
2022-03-16 17:32:23 +0000

    Update new_install_py_packages.sh

--- new_install_py_packages.sh
+++ new_install_py_packages.sh
@@ -3,3 +3,5 @@ sudo /usr/bin/anaconda/bin/conda create --prefix /usr/bin/anaconda/envs/py35new 
 sudo /usr/bin/anaconda/envs/py35new/bin/pip install azure-storage-blob==12.6.0
 
 sudo /usr/bin/anaconda/envs/py35new/bin/pip install azure-storage-file-datalake==12.2.0
+
+sudo /usr/bin/anaconda/envs/py35new/bin/pip install azure-storage==0.20.3
