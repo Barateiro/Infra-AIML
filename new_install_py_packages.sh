a9a8b4e75d6fa2a6e5d257a3ef1edff46bf1982b
Barateiro <77500351+Barateiro@users.noreply.github.com>
2022-03-16 15:20:07 +0000

    Create new_install_py_packages.sh

---
+++ new_install_py_packages.sh
@@ -0,0 +1,5 @@
+sudo /usr/bin/anaconda/bin/conda create --prefix /usr/bin/anaconda/envs/py35new python=2.7 anaconda=4.3 --yes
+
+sudo /usr/bin/anaconda/envs/py35new/bin/pip install azure-storage-blob==12.6.0
+
+sudo /usr/bin/anaconda/envs/py35new/bin/pip install azure-storage-file-datalake==12.2.0
