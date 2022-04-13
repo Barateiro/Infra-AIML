207a43a52e6587b46403bdb6dc84f690be7d0805
Barateiro <77500351+Barateiro@users.noreply.github.com>
2022-04-13 11:05:59 +0100

    Update new_install_py_packages.sh

--- new_install_py_packages.sh
+++ new_install_py_packages.sh
@@ -1,5 +1,5 @@
-sudo /usr/bin/anaconda/bin/conda create --prefix /usr/bin/anaconda/envs/newtestenv python=2.7 anaconda=4.3 --yes
+sudo /usr/bin/anaconda/bin/conda create --prefix /usr/bin/anaconda/envs/curationenv python=2.7 anaconda=4.3 --yes
 
-sudo /usr/bin/anaconda/envs/newtestenv/bin/pip install azure-storage==0.20.3
+sudo /usr/bin/anaconda/envs/curationenv/bin/pip install azure-storage==0.20.3
 
-sudo /usr/bin/anaconda/envs/newtestenvw/bin/pip install azure-storage-blob==12.6.0
+sudo /usr/bin/anaconda/envs/curationenv/bin/pip install azure-storage-blob==12.6.0
